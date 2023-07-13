fcd04c3f7544eab42867d77520577c355adb08a0
NikolaJelavic <jelavicnikola@gmail.com>
2023-07-13 16:22:34 +0200

    Create Form

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -7,7 +7,22 @@ export default async function handler(request, response) {
   if (request.method === "GET") {
     const places = await Place.find();
     return response.status(200).json(places);
-  } else {
-    return response.status(405).json({ message: "Method not allowed" });
+  } 
+  // else {
+  //   return response.status(405).json({ message: "Method not allowed" });
+  // }
+
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      const place = new Place(placeData);
+      await place.save();
+
+      response.status(201).json({ status: "Place created" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
   }
+
 }
